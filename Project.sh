5377675e32b0e7ce417e36fe927a0e5aa167b6d3
LUC1F3R818 <74072072+LUC1F3R818@users.noreply.github.com>
2020-11-19 12:29:38 +0630

    Update and rename README.md to Project.sh

---
+++ Project.sh
@@ -0,0 +1,160 @@
+#B14CK 4TT4CK3R
+
+blue='\033[33;1m'
+green='\033[37;1m'
+purple='\033[31;1m'
+cyan='\033[36;1m'
+red='\033[35;1m'
+white='\033[32;1m'
+yellow='\033[34;1m'
+sleep 1
+
+figlet 🖤0XYG3N🖤 | lolcat
+echo -e ""
+
+
+echo -e " Welcome To Tool Collection " 
+echo -e " Author    : B14CK 4TT4CK3R Team"  
+echo -e " Facebook  : Nyein Pyae(Error) " 
+echo ""
+echo -e " \e[1;30m ................ ∆ Myanmar B14CK 4TT4CK3R Team ∆............ \e[0m " 
+echo ""
+echo "                 !Easily install the desired tool! "
+echo ""
+
+echo -e "__________________________________" 
+echo -e "1.Tools Sqlmap " 
+echo "__________________________________"
+echo -e "2.Tools Socialspolit "
+echo "__________________________________"
+echo -e "3.Tools Ddos Attack " 
+echo "__________________________________"
+echo -e "4.Tools Firecrack " 
+echo "__________________________________"
+echo -e "5.Tools Termux Logo" 
+echo "__________________________________"
+echo -e "6.Tools Hack Old Acc"
+echo "__________________________________"
+echo -e "7.Tools Zphisher" 
+echo ""
+read -p "Nyein Pyae;Choose tool number!~# " asu
+
+
+if [ $asu = 1 ] || [ $asu = 1 ]
+then
+clear
+figlet 0XYG3N | lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt inatall python
+apt inatall python2
+apt install php
+git clone https://github.com/sqlmapproject/sqlmap
+chmod +x *
+mv sqlmap $HAME
+cd $HOME/sqlmap
+python2 sqlmap.py -h
+fi
+
+
+if [ $asu = 2 ] || [ $asu = 2 ]
+then
+clear
+figlet 0XYG3N | lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+apt install git
+apt install php
+apt install -y git
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+mv SocialSploit $HOME
+cd $HOME/SocialSploit
+cd SocialSploit
+bash install.sh
+./Sploit
+fi
+
+if [ $asu = 3 ] || [ $asu = 3 ]
+then
+clear
+figlet 0XYG3N | lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+apt install git
+git clone https://github.com/Techtushar07/DDos-Attack.git
+mv ddos-attack $HOME
+cd $HOME/ddos-attack
+cd DDos-Attack
+fi
+
+if [ $asu = 4 ] || [ $asu = 4 ]
+then
+clear
+figlet 0XYG3N | lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Ranginang67/Firecrack
+cd Firecrack
+pip2 install -r requirements.txt
+pip install --upgrade pip
+python2 firecrack.py
+fi
+
+if [ $asu = 5 ] || [ $asu = 5 ]
+then
+clear
+figlet 0XYG3N | lolcat
+echo  "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+git clone https://github.com/remo7777/T-Header
+cd T-Header
+bash t-header.sh
+fi
+
+if [ $asu = 6 ] || [ $asu = 6 ]
+then
+figlet 0XYG3N| lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+pkg install python
+pkg install python2
+pkg install git
+pkg install wget
+pkg install php
+pkg install openssh
+git clone https://github.com/botolmehedi/bint
+pip2 install requests
+pip2 install mechanize
+ls
+cd bint
+python2 bint.py
+fi
+
+if [ $asu = 7 ] || [ $asu = 7 ]
+then
+figlet 0XYG3N | lolcat
+echo "Rate my activity or contact our team" |lolcat
+sleep 3
+apt update
+apt upgrade
+apt install git curl phpopenssh -y
+git clone git://github.com/htr-tech/zphisher.git
+cd zphisher
+bash zphisher.sh
+fi
+
+
+wget -O rapidscan.py https://raw
--- README.md
+++
@@ -1,2 +0,0 @@
-# useful-tool
-Myanmar
